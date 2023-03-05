12ebc2af9367f2b0c105a3677bf0db323ed91fe9
Andrew Kim <83758816+andr-kim@users.noreply.github.com>
2023-03-05 16:37:15 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,32 +1,22 @@
 <?php
-$name = $_POST['user_name'];
-$email = $_POST['user_email'];
-$text = $_POST['user_text'];
+if(isset($_POST['submit'])){
+    $to = "kimovskiy@gmail.com";
+    $subject = "Новая заявка";
+    $name = $_POST['user_name'];
+    $email = $_POST['user_email'];
+    $message = $_POST['user_text'];
 
-$name = htmlspecialchars($name);
-$email = htmlspecialchars($email);
-$text = htmlspecialchars($text);
+    $headers = "From: " . $name . " <" . $email . ">\\r\\n";
+    $headers .= "Reply-To: " . $email . "\\r\\n";
 
-$name = urlencode($name);
-$email = urlencode($email);
-$text = urlencode($text);
+    $mail_body = "Имя: " . $name . "\\n";
+    $mail_body .= "Email: " . $email . "\\n";
+    $mail_body .= "Сообщение: " . $message . "\\n";
 
-$name = trim($name);
-$email = trim($email);
-$text = trim($text);
-
-if (mail("kimovskiy@gmail.com",
-     "Pest Reject",
-     "Имя: ".$name."\n".
-     "Почта ".$email,
-		 "Сообщение: ".$text,
-     "From: kimovskiy@gmail.com \r\n")
-){
-     header("Location: /ok.html");
+    if(mail($to, $subject, $mail_body, $headers)){
+        echo "Ваше сообщение успешно отправлено.";
+    } else {
+        echo "Ошибка при отправке сообщения. Пожалуйста, попробуйте еще раз.";
+    }
 }
-
-else {
-     echo ("Error");
-}
-
 ?>
